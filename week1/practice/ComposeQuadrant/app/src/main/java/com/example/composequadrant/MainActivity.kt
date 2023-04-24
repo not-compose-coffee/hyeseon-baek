84f59c85152df1a2bd1e065aca54a8bc648e6ce6
HyeseonBaek <bhs12087@naver.com>
2023-04-25 00:07:33 +0900

    [#1/feat] ComposeQuadrant 과제

--- week1/practice/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ week1/practice/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -22,29 +22,109 @@ import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            ComposeQuadrantApp()
+        }
     }
 }
 
 @Composable
-fun ComposeQuadrantApp() {
+fun ComposeQuadrantApp(){
+    ComposableQuadrant(
+        firstTitle = stringResource(R.string.first_title),
+        firstDescription = stringResource(R.string.first_description),
+        secondTitle = stringResource(R.string.second_title),
+        secondDescription = stringResource(R.string.second_description),
+        thirdTitle = stringResource(R.string.third_title),
+        thirdDescription = stringResource(R.string.third_description),
+        fourthTitle = stringResource(R.string.fourth_title),
+        fourthDescription = stringResource(R.string.fourth_description)
+    )
+}
+@Composable
+fun ComposableQuadrant(
+    modifier: Modifier = Modifier,
+    firstTitle: String,
+    firstDescription: String,
+    secondTitle: String,
+    secondDescription: String,
+    thirdTitle: String,
+    thirdDescription: String,
+    fourthTitle: String,
+    fourthDescription: String
+) {
     Column() {
-        Row() { }
-        Row() { }
+        Row(
+            modifier
+                .weight(1F)
+        ) {
+            ComposableInfoCard(
+                modifier = Modifier.weight(1F),
+                title = firstTitle,
+                description = firstDescription,
+                backgroundColor = Color.Blue
+            )
+            ComposableInfoCard(
+                modifier = Modifier.weight(1F),
+                title = secondTitle,
+                description = secondDescription,
+                backgroundColor = Color.Magenta
+            )
+        }
+        Row(
+            modifier
+                .weight(1F)
+        ) {
+            ComposableInfoCard(
+                modifier = Modifier.weight(1F),
+                title = thirdTitle,
+                description = thirdDescription,
+                backgroundColor = Color.Gray
+            )
+            ComposableInfoCard(
+                modifier = Modifier.weight(1F),
+                title = fourthTitle,
+                description = fourthDescription,
+                backgroundColor = Color.Green
+            )
+        }
     }
 }
 
 @Composable
 private fun ComposableInfoCard(
+    modifier: Modifier = Modifier,
     title: String,
     description: String,
     backgroundColor: Color,
-    modifier: Modifier = Modifier
+
 ) {
-    Column( ) { }
+    Column(
+        /*
+        Column지정 : 수직 가운데 정렬은 Arrangement,수평 가운데 정렬은 Alignment
+        Row지정 : 수직 가운데 정렬은 Alignment,수평 가운데 정렬은 Arrangement
+         */
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(all = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = description
+        )
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    ComposeQuadrantApp()
+}

056bc124625154e1f21f8b9248ba1041077f333b
HyeseonBaek <bhs12087@naver.com>
2023-04-25 21:41:16 +0900

    [#1/feat] Lemonade 과제

--- week1/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ week1/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LemonadeApp()
                 }
             }
         }
@@ -30,14 +43,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LemonadeApp(){
+    LemonadeCard()
+}
+@Composable
+fun LemonadeCard(
+    modifier: Modifier = Modifier,
+) {
+    val result = remember {
+        mutableStateOf(1)
+    }
+    var imageResource: Int = R.drawable.lemon_tree
+    var description : Int = R.string.firstTap_string
+    when (result.value) {
+        1 -> {
+            imageResource = R.drawable.lemon_tree
+            description = R.string.firstTap_string
+        }
+        2 -> {
+            imageResource = R.drawable.lemon_squeeze
+            description = R.string.secondTap_string
+        }
+        3 -> {
+            imageResource = R.drawable.lemon_drink
+            description = R.string.thirdTap_string
+        }
+        4 -> {
+            imageResource = R.drawable.lemon_restart
+            description = R.string.fourthTap_string
+        }
+    }
+
+    Column(
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(description),
+                fontSize = 18.sp,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Image(
+                painter = painterResource(imageResource),
+                contentDescription = stringResource(R.string.app_name),
+                modifier = modifier
+                    .border(
+                        width = 2.dp,
+                        shape = RoundedCornerShape(4.dp),
+                        color = colorResource(R.color.corner_color)
+                    )
+                    .clickable {
+                        if (result.value == 4) {
+                            result.value = 1
+                        } else {
+                            result.value++
+                        }
+                    }
+            )
+        }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    LemonadeTheme {
-        Greeting("Android")
-    }
+    LemonadeApp()
 }

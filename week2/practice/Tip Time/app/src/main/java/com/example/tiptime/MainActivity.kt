e176fa1b7c947b49a2f2fa7e660e84ad48a87bc7
HyeseonBaek <bhs12087@naver.com>
2023-04-30 18:07:11 +0900

    [#3/feat] set KeyboardOptions

--- week2/practice/Tip Time/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ week2/practice/Tip Time/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -63,12 +64,14 @@ fun TipTimeScreen() {
         EditNumberField(
             label = R.string.bill_amount,
             value = amountInput,
-            onValueChange = { amountInput = it }
+            onValueChanged = { amountInput = it },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next)
         )
         EditNumberField(
             label = R.string.how_was_the_service,
             value = tipInput ,
-            onValueChange = { tipInput  = it }
+            onValueChanged = { tipInput  = it },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done)
         )
         Spacer(modifier = Modifier.height(24.dp))
         Text(
@@ -83,19 +86,21 @@ fun TipTimeScreen() {
 
 @Composable
 fun EditNumberField(
+    @StringRes label: Int,
+    keyboardOptions: KeyboardOptions,
     value: String,
-    onValueChange: (String) -> Unit,
-    @StringRes label : Int
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier
 ){
     /* textfield는 material design 표현, 기본 스타일이 존재함 */
     TextField(
         label = {Text(text = stringResource(label))},
-        modifier = Modifier.
+        modifier = modifier.
             fillMaxWidth(),
         singleLine = true,
         value = value,
-        onValueChange = onValueChange,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        onValueChange = onValueChanged,
+        keyboardOptions = keyboardOptions
     )
 }
 
